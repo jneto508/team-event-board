6b66830f252f4e7e123ef712aa9306a1d858ae93
Nick <nbeiner20@gmail.com>
2026-04-29 20:58:43 -0400

    fix: prisma setup file was using incorrect notation for BetterSqLite import

--- test/setup.ts
+++ test/setup.ts
@@ -1,8 +1,8 @@
 
-import { PrismaBetterSQLite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../src/generated/prisma/client";
 
-const adapter = new PrismaBetterSQLite3({
+const adapter = new PrismaBetterSqlite3({
   url: process.env.DATABASE_URL ?? "file:./prisma/dev.db",
 });
 
