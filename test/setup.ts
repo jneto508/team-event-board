dd2170d02497b9a92fb98fd0607f6af15ef683c8
Nick <nbeiner20@gmail.com>
2026-04-29 22:47:51 -0400

    fix: import notation was causing Sqlite/Prisma errors

--- test/setup.ts
+++ test/setup.ts
@@ -1,8 +1,8 @@
 
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaBetterSQLite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaBetterSqlite3({
+const adapter = new PrismaBetterSQLite3({
   url: process.env.DATABASE_URL ?? "file:./prisma/dev.db",
 });
 
